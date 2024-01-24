8e895a71b9bbc4717284013523a1cba50b008dfb
surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-01-24 20:28:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset-btn");
+let msg=document.querySelector("#msg");
+let msgcon=document.querySelector(".msg-container");
+let newgame=document.querySelector("#new-btn");
+let turn=true;
+const wp=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+newgame.addEventListener("click",()=>{
+    location.reload();
+});
+function checker(){
+    for(let p of wp){
+        let ps0=boxes[p[0]].innerText;
+        let ps1=boxes[p[1]].innerText;
+        let ps2=boxes[p[2]].innerText;
+        if(ps1=="X" && ps1==ps2 && ps0==ps1){
+            msg.innerText='winner is X';
+            msgcon.classList.remove("hide");
+
+        }
+        if(ps1=="O" && ps1==ps2 && ps0==ps1){
+            msg.innerText='winner is O';
+            msgcon.classList.remove('hide');
+        }
+    }
+}
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turn==true){
+           box.innerText="X";
+           turn =false;
+        }
+        else{
+            box.innerText="O";
+           turn =true;
+        }
+        box.disabled=true;
+        checker();
+    });
+});
+
+resetbtn.addEventListener("click",()=>{
+    // boxes.forEach((box)=>{
+    //     box.innerText="";
+    //     box.disabled=false;
+    // })
+    location.reload();
+});
