b3cac7b254007bb5fe8844b5767c4fc33e305aa1
surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-01-25 09:13:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function checker(){
 function drawfinder(){
     let count =0;
     for(let i=0;i<9;i++){
-        if(boxes[i].innerText=="X" || boxes.innerText=="O") count++;
+        if(boxes[i].innerText=="X" || boxes[i].innerText=="O") count++;
     }
     if(count == 9){
             msg.innerText='Game is Draw';
