283d030470c773aef7c72b3a9c9fe0f8bf6b127c
surna srikanth <122173096+srikanthsurna@users.noreply.github.com>
2024-02-02 14:59:45 +0530

    cleared game over bug

--- app.js
+++ app.js
@@ -25,11 +25,19 @@ function checker(){
         if(ps1=="X" && ps1==ps2 && ps0==ps1){
             msg.innerText='winner is X';
             msgcon.classList.remove("hide");
+            boxes.forEach((box)=>{
+                     box.disabled=true;
+                });
+
 
         }
         if(ps1=="O" && ps1==ps2 && ps0==ps1){
             msg.innerText='winner is O';
             msgcon.classList.remove('hide');
+            boxes.forEach((box)=>{
+                     box.disabled=true;
+                });
+
         }
     }
 }
